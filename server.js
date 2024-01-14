d6f96fe43934b7660199cd28e2f7a5debac052c8
sucre2020 <ettasucre@gmail.com>
2024-01-14 02:46:02 -0700

    Added second route

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello, This is Efita Effiom. I'm reworking/starting my week one assignment");
+  res.send("Hello, This is Efita Effiom. I'm reworking/starting my week one assignment. Happy to be back");
 });
+
+ 
+app.get('/efita', (req, res) => {
+    res.send("Efita Effiom");
+  });
  
 const port = 3000;
 
