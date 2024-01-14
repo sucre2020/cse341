cadd15eabe8982e8db6494852ce623ea26b2d285
sucre2020 <ettasucre@gmail.com>
2024-01-14 04:09:13 -0700

    added controller

---
+++ controllers/lesson.js
@@ -0,0 +1,12 @@
+const efitaRoute = (req, res) => {
+    res.send("Efita Effiom");
+  };
+
+  const lafuljiRoute = (req, res) => {
+    res.send("Lafulji Effiom");
+  };
+
+  module.exports = {
+        efitaRoute,
+        lafuljiRoute
+    };
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const app = express();
+
+const path = require('./controllers/lesson');
+
+app.get('/', path.efitaRoute);
+app.get('/lafulji', path.lafuljiRoute);
  
-app.get('/', (req, res) => {
-  res.send("Efita Effiom");
-});
+// app.get('/', (req, res) => {
+//   res.send("Efita Effiom");
+// });
 
  
-app.get('/efita', (req, res) => {
-    res.send("Lafulji Effiom");
-  });
+// app.get('/efita', (req, res) => {
+//     res.send("Lafulji Effiom");
+//   });
+
+
  
 const port = 3000;
 
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
+
