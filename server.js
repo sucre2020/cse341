36d4e4598d697e9ca2eef15551bc628073da75c7
sucre2020 <ettasucre@gmail.com>
2024-03-10 14:50:07 -0600

    updates

--- server.js
+++ server.js
@@ -14,25 +14,19 @@ const postsRoutes = require('./routes/posts');
 // Use the posts routes
 app.use('/posts', postsRoutes);
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
-
-app.get('/post', (req, res) => {
-    res.send('We are on post');
-});
-
 // Connect to MongoDB
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yoczwia.mongodb.net/contact?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(uri, {})
     .then(() => {
-        console.log('Connected to MongoDB!');
+        console.log('Connected to MongoDB Successfully!');
         
+        // Set the port from the environment variable or default to 3000
+        const port = process.env.PORT || 3000;
+
         // Start the server after successfully connecting to MongoDB
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
         });
     })
     .catch((error) => {
