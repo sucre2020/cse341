9efc414a2041553102465874159f30dc41dc84dc
sucre2020 <ettasucre@gmail.com>
2024-03-10 12:36:08 -0600

    changed connection string to since render rejected the first one

--- server.js
+++ server.js
@@ -24,17 +24,15 @@ app.get('/post', (req, res) => {
 });
 
 // Connect to MongoDB
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yoczwia.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.connect(uri, {})
-    .then(() => {
-        console.log('Connected to MongoDB!');
-        
-        // Start the server after successfully connecting to MongoDB
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
+mongoose.connect('mongodb://localhost:27017/test')
+  .then(() => {
+    console.log('Connected to MongoDB!');
+    
+    // Start the server after successfully connecting to MongoDB
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
     });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
