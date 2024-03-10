5b11f93578e715c7ed15a3f2952b87d43462c16a
sucre2020 <ettasucre@gmail.com>
2024-03-10 13:02:46 -0600

    reverted back to mongodb+srv conection string to see if render will be happy

--- server.js
+++ server.js
@@ -24,15 +24,17 @@ app.get('/post', (req, res) => {
 });
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/test')
-  .then(() => {
-    console.log('Connected to MongoDB!');
-    
-    // Start the server after successfully connecting to MongoDB
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.yoczwia.mongodb.net/contact?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(uri, {})
+    .then(() => {
+        console.log('Connected to MongoDB!');
+        
+        // Start the server after successfully connecting to MongoDB
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
     });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
